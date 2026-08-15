555b8a970d5ca81a3ab0e83a8a72013f702879ee
David-Stratulat <dstratulat258@gmail.com>
2026-08-15 23:04:24 +0300

    chore(deploy): configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Ladaniore_Power_Grup/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
